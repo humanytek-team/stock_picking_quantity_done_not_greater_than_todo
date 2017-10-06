0871e9775c341eaff8f9a9e83eab402808ca1a6f
Manuel Marquez <buzondemam@gmail.com>
2017-10-06 15:14:48 -0400

    [ADD] Adds constrain in model stock.pack.operation to restrict that stock users (not stock managers) can set a quantity done greater that quantity to do in lines of products to receive / deliver in stock pickings.

---
+++ README.rst
@@ -0,0 +1,1 @@
+This module restricts that stock users (not stock managers) can set a quantity done greater that quantity to do in lines of products to receive / deliver in stock pickings.
---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Humanytek - Manuel Marquez <manuel@humanytek.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Validate quantity done / to do in lines of stock pickings',
+    'version': '9.0.0.1.0',
+    'category': 'Stock',
+    'author': 'Humanytek',
+    'website': "http://www.humanytek.com",
+    'license': 'AGPL-3',
+    'depends': ['stock', ],
+    'data': [        
+    ],
+    'installable': True,
+    'auto_install': False
+}
